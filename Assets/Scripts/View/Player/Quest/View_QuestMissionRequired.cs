31476e6498e55510550fdc799738054a2b0ab4d1
xxslit <xxslit@Gmail.com>
2018-04-03 18:49:47 +0800

    View_QuestMissionRequired
    任务需求VIEW

---
+++ Assets/Scripts/View/Player/Quest/View_QuestMissionRequired.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_QuestMissionRequired : MonoBehaviour
+{
+    [SerializeField]private Text currentCount;
+    [SerializeField]private Text questCount;
+    [SerializeField]private Text questName;
+
+    public Text CurrentCount
+    {
+        get { return currentCount; }
+
+        set { currentCount = value; }
+    }
+
+    public Text QuestCount
+    {
+        get { return questCount; }
+
+        set { questCount = value; }
+    }
+
+    public Text QuestName
+    {
+        get { return questName; }
+
+        set { questName = value; }
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
