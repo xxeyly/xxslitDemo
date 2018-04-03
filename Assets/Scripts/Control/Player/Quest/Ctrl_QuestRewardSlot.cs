833ef89df355cded10d78ee2c77e7eeafd9ae7a2
xxslit <xxslit@Gmail.com>
2018-04-03 18:43:48 +0800

    Ctrl_QuestRewardSlot
    奖励的物品格子
    处理 经验 钻石 金币等显示

---
+++ Assets/Scripts/Control/Player/Quest/Ctrl_QuestRewardSlot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_QuestRewardSlot : MonoBehaviour
+{
+    private Model_Item item;
+
+    public Model_Item Item
+    {
+        get { return item; }
+
+        set
+        {
+            item = value;
+            if (value != null)
+            {
+                GetComponent<View_QuestRewardSlot>().InitSlot();
+            }
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    public void ShowGoldView(int goldCount)
+    {
+        GetComponent<View_QuestRewardSlot>().ShowGoldView(goldCount);
+    }
+
+    public void ShowDiamodView(int diamodCount)
+    {
+        GetComponent<View_QuestRewardSlot>().ShowDiamodView(diamodCount);
+    }
+
+    public void ShowExpView(int expCount)
+    {
+        GetComponent<View_QuestRewardSlot>().ShowExpView(expCount);
+    }
+
+    void Update()
+    {
+    }
+}
