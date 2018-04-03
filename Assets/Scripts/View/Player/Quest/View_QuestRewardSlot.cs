e009d8f8f12c91acffe00efe6c4f705eecccb688
xxslit <xxslit@Gmail.com>
2018-04-03 18:50:13 +0800

    View_QuestRewardSlot

---
+++ Assets/Scripts/View/Player/Quest/View_QuestRewardSlot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class View_QuestRewardSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Text itemCount;
+    [SerializeField] private GameObject hoverOverlay;
+
+    public void InitSlot()
+    {
+        icon.sprite = Resources.Load<Sprite>(GetComponent<Ctrl_QuestRewardSlot>().Item.sprite);
+        itemCount.text = GetComponent<Ctrl_QuestRewardSlot>().Item.currentNumber.ToString();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hoverOverlay.SetActive(true);
+        if (GetComponent<Ctrl_QuestRewardSlot>().Item == null)
+        {
+            return;
+        }
+        Ctrl_InventoryManager.Instance.isToolTipShow = true;
+        Ctrl_InventoryManager.Instance.Tootip.GetComponent<View_ToolTip>()
+            .Show(GetComponent<Ctrl_QuestRewardSlot>().Item.ItemInfo());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hoverOverlay.SetActive(false);
+        Ctrl_InventoryManager.Instance.isToolTipShow = false;
+    }
+
+    public void ShowGoldView(int amount)
+    {
+        icon.sprite = Resources.Load<Sprite>("Sprites/Items/Currency_Gold");
+        itemCount.text = amount.ToString();
+    }
+
+    public void ShowDiamodView(int amount)
+    {
+        icon.sprite = Resources.Load<Sprite>("Sprites/Items/gem");
+        itemCount.text = amount.ToString();
+    }
+
+    public void ShowExpView(int amount)
+    {
+        icon.sprite = Resources.Load<Sprite>("Sprites/Items/exp");
+        itemCount.text = amount.ToString();
+    }
+}
