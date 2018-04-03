23816442b34831bdabed8ee532b3e62312154d88
xxslit <xxslit@Gmail.com>
2018-04-03 18:49:18 +0800

    View_QuestItem

---
+++ Assets/Scripts/View/Player/Quest/View_QuestItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class View_QuestItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject activeOverlay;
+    [SerializeField] private GameObject selectOverlay;
+
+    public GameObject SelectOverlay
+    {
+        get { return selectOverlay; }
+
+        set { selectOverlay = value; }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        activeOverlay.SetActive(false);
+    }
+
+    void Start()
+    {
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!SelectOverlay.activeSelf)
+        {
+            activeOverlay.SetActive(true);
+        }
+    }
+
+    public void ShowSelectOverlay()
+    {
+        Ctrl_QuestItemManager.Instance.HideAllSelectOverlay();
+        SelectOverlay.SetActive(true);
+    }
+}
