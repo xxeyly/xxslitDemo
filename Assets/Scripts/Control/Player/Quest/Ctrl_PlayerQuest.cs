809477514a6da9b8a4ddee63d18e10ae98b7ece4
xxslit <xxslit@Gmail.com>
2018-04-03 11:49:34 +0800

    Ctrl_PlayerQuest
    角色任务状态记录类 判断当前任务的状态

---
+++ Assets/Scripts/Control/Player/Quest/Ctrl_PlayerQuest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_PlayerQuest : Singleton<Ctrl_PlayerQuest>
+{
+    private List<Model_Quest> playQuestList;
+
+    public List<Model_Quest> PlayQuestList
+    {
+        get { return playQuestList; }
+
+        set { playQuestList = value; }
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        playQuestList = new List<Model_Quest>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (PlayQuestList.Count >= 1)
+        {
+            Debug.Log(PlayQuestList[0].questKillEnemy[0].enemyNumber);
+            Debug.Log(PlayQuestList[0].questKillEnemy[0].currentEnemyNumber);
+        }
+    }
+}
