01cda55bff303d64c72f1433863ef4d62b48732f
xxslit <xxslit@Gmail.com>
2018-04-03 18:39:47 +0800

    Ctrl_QuestItem
    queetItem点击事件

---
+++ Assets/Scripts/Control/Player/Quest/Ctrl_QuestItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Ctrl_QuestItem : MonoBehaviour, IPointerDownHandler
+{
+    private Model_Quest quest;
+
+    public Model_Quest Quest
+    {
+        get { return quest; }
+
+        set
+        {
+            quest = value;
+            Ctrl_QuestItemManager.Instance.ShowQuestInfo(quest);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            GetComponent<View_QuestItem>().ShowSelectOverlay();
+        }
+    }
+}
