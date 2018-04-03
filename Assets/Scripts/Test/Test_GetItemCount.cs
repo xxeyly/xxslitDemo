a41b9218a76d94f692fd7b7c053d99c248720502
xxslit <xxslit@Gmail.com>
2018-04-03 18:47:59 +0800

    Test_GetItemCount
    测试脚本 获得当前背包有多少个当前的物品

---
+++ Assets/Scripts/Test/Test_GetItemCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_GetItemCount : MonoBehaviour {
+
+    [SerializeField] private InputField ItemId;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GetItemCount()
+    {
+        int itemID = Int32.Parse(ItemId.text);
+        Debug.Log(Ctrl_InventoryManager.Instance.GetItemCount(itemID));
+    }
+
+}
