60feb69e6f041f818bb9f102042c5c9d59f27dd6
xxslit <xxslit@Gmail.com>
2018-04-05 10:01:40 +0800

    Ctrl_NPCQuest
    点击NPC时增加一个判断,如果执行的任务中有跟这个npc谈话的细节,优先播放这个的对话

--- Assets/Scripts/Control/NPC/Ctrl_NPCQuest.cs
+++ Assets/Scripts/Control/NPC/Ctrl_NPCQuest.cs
@@ -6,6 +6,7 @@ public class Ctrl_NPCQuest : MonoBehaviour
 {
     private List<Model_Quest> questList;
     [SerializeField] private int[] quests;
+    [SerializeField] private int npcId;
 
     public int[] Quests
     {
@@ -20,6 +21,7 @@ public class Ctrl_NPCQuest : MonoBehaviour
 
         set { questList = value; }
     }
+
     /// <summary>
     /// 初始化任务数据
     /// </summary>
@@ -38,6 +40,28 @@ public class Ctrl_NPCQuest : MonoBehaviour
     /// </summary>
     private void OnMouseDown()
     {
+        //如果接受的任务中有关于这个npc谈话的内容
+        foreach (Model_Quest quest in Ctrl_PlayerQuest.Instance.PlayQuestList)
+        {
+            foreach (Model_Quest.QuestNPC npc in quest.questTalkNPC)
+            {
+                if (!npc.isTalk)
+                {
+                    if (npc.npcId == npcId)
+                    {
+                        npc.isTalk = true;
+                        Ctrl_TootipManager.Instance.ShowQuest();
+                        Ctrl_TootipManager.Instance.TalkInMission(npc);
+                        if (Ctrl_QuestItemManager.Instance.GetSelectOverlay().Quest.id == quest.id)
+                        {
+                            Ctrl_QuestItemManager.Instance.ShowQuestInfo(quest);
+                        }
+                    }
+                }
+            }
+        }
+
+        //如果当前任务不是已提交状态
         foreach (Model_Quest quest in QuestList)
         {
             if (!quest.questSubmit)
@@ -50,6 +74,5 @@ public class Ctrl_NPCQuest : MonoBehaviour
 
     private void Update()
     {
-
     }
 }
