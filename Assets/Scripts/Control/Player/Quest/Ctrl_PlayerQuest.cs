ee5d99aec412ddc124a4f3bb6e903ef1165cdf1e
xxslit <xxslit@Gmail.com>
2018-04-05 10:03:58 +0800

    Ctrl_PlayerQuest
    任务状态实时更新检测
    如果当前任务列表有任务就执行一个协成 实时检测任务中所需道具(击杀,道具,谈话)等是否满足,都满足的话判定当前任务为完成状态

--- Assets/Scripts/Control/Player/Quest/Ctrl_PlayerQuest.cs
+++ Assets/Scripts/Control/Player/Quest/Ctrl_PlayerQuest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Ctrl_PlayerQuest : Singleton<Ctrl_PlayerQuest>
 {
-    private List<Model_Quest> playQuestList;
+    private List<Model_Quest> playQuestList;//所有接受的任务,在任务对话时只有接受任务才会存储
 
     public List<Model_Quest> PlayQuestList
     {
@@ -19,17 +19,123 @@ public class Ctrl_PlayerQuest : Singleton<Ctrl_PlayerQuest>
         playQuestList = new List<Model_Quest>();
     }
 
-    void Start()
+    private void Start()
     {
-
     }
 
-    void Update()
+    private void Update()
     {
-        if (PlayQuestList.Count >= 1)
+        StartCoroutine(LoopCheckQuest());
+    }
+
+    /// <summary>
+    /// 每隔1s检测任务当前是否完成
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator LoopCheckQuest()
+    {
+        while (PlayQuestList.Count >= 1)
         {
-            Debug.Log(PlayQuestList[0].questKillEnemy[0].enemyNumber);
-            Debug.Log(PlayQuestList[0].questKillEnemy[0].currentEnemyNumber);
+            yield return new WaitForSeconds(1f);
+            foreach (Model_Quest quest in PlayQuestList)
+            {
+                //已经是完成状态的任务不参与
+                if (!quest.questSubmit)
+                {
+                    bool skillOver = false;//任务击杀目标是否完成
+                    foreach (Model_Quest.QuestEnemy enemy in quest.questKillEnemy)
+                    {
+                        if (enemy.enemyId != -1)
+                        {
+                            if (enemy.currentEnemyNumber >= enemy.enemyNumber)
+                            {
+                                skillOver = true;
+                            }
+                            else
+                            {
+                                skillOver = false;
+                            }
+                        }
+                        else
+                        {
+                            skillOver = true;
+                        }
+                    }
+
+                    bool talkOver = false;//任务谈话是否完成
+                    foreach (Model_Quest.QuestNPC npc in quest.questTalkNPC)
+                    {
+                        if (npc.npcId != -1)
+                        {
+                            if (npc.isTalk)
+                            {
+                                talkOver = true;
+                            }
+                            else
+                            {
+                                talkOver = false;
+                            }
+                        }
+                        else
+                        {
+                            talkOver = true;
+                        }
+                    }
+
+                    bool itemOver = false;//任务寻找物品是否完成
+                    foreach (Model_Quest.QuestItem item in quest.questItem)
+                    {
+                        if (item.itemId != -1)
+                        {
+                            if (Ctrl_InventoryManager.Instance.GetItemCount(item.itemId) >= item.itemNumber)
+                            {
+                                itemOver = true;
+                            }
+                            else
+                            {
+                                itemOver = false;
+                            }
+                        }
+                        else
+                        {
+                            itemOver = true;
+                        }
+                    }
+
+//                    Debug.Log("击杀:" + skillOver + "谈话:" + talkOver + "物品" + itemOver);
+                    if (skillOver && talkOver && itemOver)
+                    {
+                        quest.questState = true;
+                        //如果当前选择显示的任务跟已完成的任务相同 显示任务完成按钮
+                        if (Ctrl_QuestItemManager.Instance.GetSelectOverlay().Quest.id == quest.id)
+                        {
+                            Ctrl_QuestItemManager.Instance.ShowOver();
+                        }
+
+                        //循环遍历所有的任务 如果跟完成的任务id相同 判断已经完成任务
+                        foreach (Ctrl_QuestItem questItem in Ctrl_QuestItemManager.Instance.QuestItems)
+                        {
+                            //如果ID相同 设置当前任务为完成状态
+                            if (quest.id == questItem.Quest.id)
+                            {
+                                questItem.SHOverMaker(true);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        quest.questState = false;//如果当前任务未完成,隐藏任务完成的标识
+                        foreach (Ctrl_QuestItem questItem in Ctrl_QuestItemManager.Instance.QuestItems)
+                        {
+                            //如果ID相同 设置当前任务为完成状态
+                            if (quest.id == questItem.Quest.id)
+                            {
+                                questItem.SHOverMaker(false);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
