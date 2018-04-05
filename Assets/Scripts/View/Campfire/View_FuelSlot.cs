0116355bf7819b20918db8967df58330a5b5b58e
xxslit <xxslit@Gmail.com>
2018-04-05 23:44:27 +0800

    View_FuelSlot
    燃料格子 UI初始化 UI物品个数的更新

---
+++ Assets/Scripts/View/Campfire/View_FuelSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_FuelSlot : MonoBehaviour
+{
+    [SerializeField] private GameObject icon;
+    [SerializeField] private Text count;
+
+    public void InitView(Model_Item item)
+    {
+        if (item != null)
+        {
+            icon.gameObject.SetActive(true);
+            icon.GetComponent<Image>().sprite = Resources.Load<Sprite>(item.sprite);
+            count.text = item.currentNumber.ToString();
+        }
+        else
+        {
+            icon.gameObject.SetActive(false);
+            count.text = " ";
+        }
+    }
+
+    public void UpdateAmount(Model_Item item)
+    {
+        count.text = item.currentNumber.ToString();
+    }
+}
