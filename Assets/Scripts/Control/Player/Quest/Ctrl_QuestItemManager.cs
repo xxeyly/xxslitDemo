bec3e3f5b23562a2128956b30b2a5dadbf230c01
xxslit <xxslit@Gmail.com>
2018-04-05 10:06:38 +0800

    Ctrl_QuestItemManager
    增加注释

--- Assets/Scripts/Control/Player/Quest/Ctrl_QuestItemManager.cs
+++ Assets/Scripts/Control/Player/Quest/Ctrl_QuestItemManager.cs
@@ -4,61 +4,139 @@ using UnityEngine;
 
 public class Ctrl_QuestItemManager : Singleton<Ctrl_QuestItemManager>
 {
-    [SerializeField] private Sprite downSprite;
-    [SerializeField] private Sprite upSprite;
-    private View_QuestItem[] questItems;
+    private Ctrl_QuestItem[] questItems;
     [SerializeField] private GameObject questInfoGo;
     [SerializeField] private Transform questItemsTransform;
     private View_QuestInfo questInfo;
 
+    public Ctrl_QuestItem[] QuestItems
+    {
+        get { return questItems; }
+
+        set { questItems = value; }
+    }
+
     void Start()
     {
-        questItems = transform.GetComponentsInChildren<View_QuestItem>();
+//        questItems = transform.GetComponentsInChildren<View_QuestItem>();
         questInfo = this.questInfoGo.GetComponent<View_QuestInfo>();
     }
 
-    public View_QuestItem GetSelectOverlay()
+    /// <summary>
+    /// 获得选中状态的任务
+    /// </summary>
+    /// <returns></returns>
+    public Ctrl_QuestItem GetSelectOverlay()
     {
-        if (questItems != null)
+        if (QuestItems != null)
         {
-            foreach (View_QuestItem questItem in questItems)
+            foreach (Ctrl_QuestItem questItem in QuestItems)
             {
-                if (questItem.SelectOverlay.activeSelf)
+                if (questItem.GetSelectOverlay())
                 {
                     return questItem;
                 }
             }
         }
 
-
         return null;
     }
 
+    /// <summary>
+    /// 隐藏所有选中状态
+    /// </summary>
     public void HideAllSelectOverlay()
     {
-        if (questItems != null)
+        if (QuestItems != null)
         {
-            foreach (View_QuestItem questItem in questItems)
+            foreach (Ctrl_QuestItem questItem in GetQuestItem())
             {
-                questItem.SelectOverlay.SetActive(false);
+                questItem.SetSelectOverlay(false);
             }
         }
     }
 
+    /// <summary>
+    /// 任务详情赋值
+    /// </summary>
+    /// <param name="quest"></param>
     public void ShowQuestInfo(Model_Quest quest)
     {
+        questInfoGo.SetActive(true);
         questInfo.Quest = quest;
     }
-
+    /// <summary>
+    /// 显示任务完成
+    /// </summary>
+    public void ShowOver()
+    {
+        questInfo.ShowOver();
+    }
+    /// <summary>
+    /// 更新QuestItem列表,接受任务时更新
+    /// </summary>
     public void UpdateQuestItem()
     {
         foreach (Model_Quest quest in Ctrl_PlayerQuest.Instance.PlayQuestList)
         {
-            GameObject questItem = Resources.Load<GameObject>("Prefabs/UI/Quest/Quest");
-            questItem.GetComponent<Ctrl_QuestItem>().Quest = quest;
-            questItem = Instantiate(questItem);
-            questItem.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
-            questItem.transform.parent = questItemsTransform;
+            //如果添加的任务Item没有存在,则可以继续添加
+            if (!IsSameExistenceQuest(quest))
+            {
+                GameObject questItem = Resources.Load<GameObject>("Prefabs/UI/Quest/Quest");
+                questItem = Instantiate(questItem);
+                questItem.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                questItem.transform.parent = questItemsTransform;
+                questItem.GetComponent<Ctrl_QuestItem>().Quest = quest;
+            }
+            //任务初始化 如果只接了一个任务 默认显示第一个任务
+            if (GetQuestItem().Length == 1)
+            {
+                ShowQuestInfo(quest);
+                GetQuestItem()[0].SetSelectOverlay(true);
+            }
         }
+    }
+
+    /// <summary>
+    /// 清空当前的QuestItem
+    /// </summary>
+    public void ClearQuestItem(Model_Quest quest)
+    {
+        foreach (Ctrl_QuestItem questItem in QuestItems)
+        {
+            if (questItem.Quest.id == quest.id)
+            {
+                Destroy(questItem.gameObject);
+            }
+        }
+        questInfoGo.SetActive(false);
+    }
+
+    /// <summary>
+    /// 获得所有已接受任务的Item
+    /// </summary>
+    /// <returns></returns>
+    private Ctrl_QuestItem[] GetQuestItem()
+    {
+        return QuestItems = transform.GetComponentsInChildren<Ctrl_QuestItem>();
+    }
+
+    /// <summary>
+    /// 是否存在相同的任务 
+    /// </summary>
+    /// <param name="quest"></param>
+    /// <returns></returns>
+    private bool IsSameExistenceQuest(Model_Quest quest)
+    {
+        bool isExist = false;
+        foreach (var QuestItem in GetQuestItem())
+        {
+            if (QuestItem.Quest.id == quest.id)
+            {
+                return isExist = true;
+            }
+        }
+
+        return isExist;
     }
 }
