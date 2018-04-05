553b5801ce7037644e2d69be0491a3096f430449
xxslit <xxslit@Gmail.com>
2018-04-05 23:39:50 +0800

    Ctrl_CampfireManager
    烹饪管理器
    增加开火与关火功能,只有有木材的时候才能执行
    开火的时候执行一个协程 根据每个木材的消耗时间去自减1

---
+++ Assets/Scripts/Control/Campfire/Ctrl_CampfireManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ctrl_CampfireManager : Singleton<Ctrl_CampfireManager>
+{
+    [SerializeField] private Ctrl_FuelSlot fuelSlot;
+    [SerializeField] private Button switchBtn;
+    private bool isCombustion;
+
+    public bool IsCombustion
+    {
+        get { return isCombustion; }
+
+        set { isCombustion = value; }
+    }
+
+    public void OnSwitch()
+    {
+        if (fuelSlot.Item != null)
+        {
+            isCombustion = !isCombustion;
+            if (isCombustion)
+            {
+                switchBtn.GetComponentInChildren<Text>().text = "关火";
+                StartCoroutine(Combustion());
+            }
+            else
+            {
+                switchBtn.GetComponentInChildren<Text>().text = "开火";
+                StopAllCoroutines();
+            }
+        }
+        else
+        {
+            Ctrl_TootipManager.Instance.ShowNotification("你没有添加燃料!");
+        }
+    }
+
+    IEnumerator Combustion()
+    {
+        yield return new WaitForSeconds(fuelSlot.Item.consumption);
+        fuelSlot.Item.currentNumber -= 1;
+        if (fuelSlot.Item.currentNumber <= 0)
+        {
+            fuelSlot.Item = null;
+            switchBtn.GetComponentInChildren<Text>().text = "开火";
+            IsCombustion = false;
+        }
+        else
+        {
+            fuelSlot.UpdateAmount(fuelSlot.Item);
+            StartCoroutine(Combustion());
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(IsCombustion);
+    }
+}
