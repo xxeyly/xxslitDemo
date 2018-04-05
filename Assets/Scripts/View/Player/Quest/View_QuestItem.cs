b789d084a77451e254c16d9b7ff9d6e04136fd2f
xxslit <xxslit@Gmail.com>
2018-04-05 10:20:06 +0800

    View_QuestItem
    增加注释
    显示和隐藏完成任务

--- Assets/Scripts/View/Player/Quest/View_QuestItem.cs
+++ Assets/Scripts/View/Player/Quest/View_QuestItem.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class View_QuestItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private GameObject activeOverlay;
-    [SerializeField] private GameObject selectOverlay;
+    [SerializeField] private GameObject activeOverlay; //滑动显示
+    [SerializeField] private GameObject selectOverlay; //选中显示
+    [SerializeField] private Text questItemText; //任务名称
+    [SerializeField] private GameObject questOver; //任务完成标志
 
     public GameObject SelectOverlay
     {
@@ -14,16 +17,26 @@ public class View_QuestItem : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
         set { selectOverlay = value; }
     }
-
+    /// <summary>
+    /// 显示任务Item的显示名称
+    /// </summary>
+    public void InitQuestItem()
+    {
+        questItemText.text = GetComponent<Ctrl_QuestItem>().Quest.questBriefIntroduction;
+    }
+    /// <summary>
+    /// 滑出取消选中标识
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerExit(PointerEventData eventData)
     {
         activeOverlay.SetActive(false);
     }
 
-    void Start()
-    {
-    }
-
+    /// <summary>
+    /// 滑动显示
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (!SelectOverlay.activeSelf)
@@ -32,9 +45,20 @@ public class View_QuestItem : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         }
     }
 
+    /// <summary>
+    /// 选中显示标识(只有选中才会显示)
+    /// </summary>
     public void ShowSelectOverlay()
     {
         Ctrl_QuestItemManager.Instance.HideAllSelectOverlay();
         SelectOverlay.SetActive(true);
+    }
+    /// <summary>
+    ///   显示隐藏任务已完成的标识,默认隐藏
+    /// </summary>
+    /// <param name="value"></param>
+    public void SHOverMaker(bool value)
+    {
+        questOver.SetActive(value);
     }
 }
