aa1027a328db5b9dbe6c0d2d40f4bca3944b8073
xxslit <xxslit@Gmail.com>
2018-04-05 10:05:48 +0800

    Ctrl_QuestItem
    任务列表增加一个选中标识
    增加3个方法
    GetSelectOverlay 获得当前QuestItem是否是选中状态
    SetSelectOverlay 设置选中和不选中
    SHOverMaker 显示隐藏任务已完成的标识,默认隐藏

--- Assets/Scripts/Control/Player/Quest/Ctrl_QuestItem.cs
+++ Assets/Scripts/Control/Player/Quest/Ctrl_QuestItem.cs
@@ -14,15 +14,47 @@ public class Ctrl_QuestItem : MonoBehaviour, IPointerDownHandler
         set
         {
             quest = value;
-            Ctrl_QuestItemManager.Instance.ShowQuestInfo(quest);
+            GetComponent<View_QuestItem>().InitQuestItem();
         }
     }
 
+    /// <summary>
+    /// 鼠标按下,显示按下效果 并把当前任务详情复制给任务显示面板
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerDown(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
             GetComponent<View_QuestItem>().ShowSelectOverlay();
+            Ctrl_QuestItemManager.Instance.ShowQuestInfo(Quest);
         }
+    }
+
+    /// <summary>
+    /// 获得当前QuestItem是否是选中状态
+    /// </summary>
+    /// <returns></returns>
+    public bool GetSelectOverlay()
+    {
+        return GetComponent<View_QuestItem>().SelectOverlay.activeSelf;
+    }
+
+    /// <summary>
+    /// 设置选中和不选中
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetSelectOverlay(bool value)
+    {
+        GetComponent<View_QuestItem>().SelectOverlay.SetActive(value);
+    }
+
+    /// <summary>
+    /// 显示隐藏任务已完成的标识,默认隐藏
+    /// </summary>
+    /// <param name="value"></param>
+    public void SHOverMaker(bool value)
+    {
+        GetComponent<View_QuestItem>().SHOverMaker(value);
     }
 }
