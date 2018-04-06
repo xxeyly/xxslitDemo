3a8a1196d28c6699e307378922dbf72d122e0ba3
xxslit <xxslit@Gmail.com>
2018-04-06 19:15:41 +0800

    View_FuelSlot
    火源格子的UI初始化以及UI个数更新还有开火关火显示

--- Assets/Scripts/View/Campfire/View_FuelSlot.cs
+++ Assets/Scripts/View/Campfire/View_FuelSlot.cs
@@ -7,7 +7,11 @@ public class View_FuelSlot : MonoBehaviour
 {
     [SerializeField] private GameObject icon;
     [SerializeField] private Text count;
-
+    [SerializeField] private Button switchBtn;
+    /// <summary>
+    /// UI初始化
+    /// </summary>
+    /// <param name="item"></param>
     public void InitView(Model_Item item)
     {
         if (item != null)
@@ -22,9 +26,26 @@ public class View_FuelSlot : MonoBehaviour
             count.text = " ";
         }
     }
-
+    /// <summary>
+    /// 更新UI个数
+    /// </summary>
+    /// <param name="item"></param>
     public void UpdateAmount(Model_Item item)
     {
         count.text = item.currentNumber.ToString();
+    }
+    /// <summary>
+    /// 更新开火/关火显示
+    /// </summary>
+    public void UpdateSwitch()
+    {
+        if (Ctrl_CampfireManager.Instance.IsCombustion)
+        {
+            switchBtn.GetComponentInChildren<Text>().text = "关火";
+        }
+        else
+        {
+            switchBtn.GetComponentInChildren<Text>().text = "开火";
+        }
     }
 }
