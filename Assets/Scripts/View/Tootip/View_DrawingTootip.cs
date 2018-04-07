77ff731984a540bbb3d8f2b529e11e677c8d3565
xxslit <xxslit@Gmail.com>
2018-04-07 14:21:33 +0800

    View_DrawingTootip
    图纸显示类

---
+++ Assets/Scripts/View/Tootip/View_DrawingTootip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_DrawingTootip : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text DescribeOutside; //外面的文本控制大小
+    [SerializeField] private Text DescribeInside; //里面的文本控制显示
+    [SerializeField] private Text EquipmentAttributeOutside; //装备/物品属性外围控制大小
+    [SerializeField] private Text EquipmentAttributeInside; //装备/物品属性里面控制大小
+    [SerializeField] private Text MaterialConsumptionOutside; //外面的文本控制大小
+    [SerializeField] private Text MaterialConsumptionInside; //里面的文本控制显示
+
+    public void InitView(Model_Item item)
+    {
+        icon.sprite = Resources.Load<Sprite>(item.sprite);
+        itemName.text = item.itemName;
+        DescribeOutside.text = item.description + "\n\n";
+        DescribeInside.text = item.description;
+        EquipmentAttributeOutside.text = item.EquipTootip() + item.ConsumableTootip() + "\n\n";
+        EquipmentAttributeInside.text = item.EquipTootip() + item.ConsumableTootip();
+
+    }
+}
