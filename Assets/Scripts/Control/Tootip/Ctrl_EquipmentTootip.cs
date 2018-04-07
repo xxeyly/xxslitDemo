462b1d407e207aafb0be66fcf021ed1bbfec51f0
xxslit <xxslit@Gmail.com>
2018-04-07 14:13:17 +0800

    Ctrl_EquipmentTootip
    装备/物品弹窗类 负责将装备/消耗品传送给View_EquipmentTootip,进行显示

---
+++ Assets/Scripts/Control/Tootip/Ctrl_EquipmentTootip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_EquipmentTootip : MonoBehaviour
+{
+
+    private Model_Item item;
+
+    public Model_Item Item
+    {
+        get
+        {
+            return item;
+        }
+
+        set
+        {
+            item = value;
+            GetComponent<View_EquipmentTootip>().InitView(value);
+        }
+    }
+}
