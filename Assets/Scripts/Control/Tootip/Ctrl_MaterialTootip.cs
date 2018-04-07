2400f11beb092f20a3059e0c08a85baa2748ea2d
xxslit <xxslit@Gmail.com>
2018-04-07 14:14:02 +0800

    Ctrl_MaterialTootip
    材料弹窗类 负责将材料传送给View_MaterialTootip,进行显示

---
+++ Assets/Scripts/Control/Tootip/Ctrl_MaterialTootip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_MaterialTootip : MonoBehaviour
+{
+    private Model_Item item;
+
+    public Model_Item Item
+    {
+        get { return item; }
+
+        set
+        {
+            item = value;
+            GetComponent<View_MaterialTootip>().InitView(value);
+        }
+    }
+}
