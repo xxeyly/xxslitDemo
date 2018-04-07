13582e1e8faf12cafbc05fbda275b251654d2b24
xxslit <xxslit@Gmail.com>
2018-04-07 15:31:06 +0800

    Test_AutoSizeHight
    测试脚本 根据字符串行数自动增加长度

---
+++ Assets/Scripts/Test/Test_AutoSizeHight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_AutoSizeHight : MonoBehaviour
+{
+    [SerializeField] private Text Content;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<RectTransform>().sizeDelta = new Vector2(330, Content.preferredHeight+30);
+    }
+}
