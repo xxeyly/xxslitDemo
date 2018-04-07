d4a931a8ca54c5627fd972e81f98483c53804acc
xxslit <xxslit@Gmail.com>
2018-04-07 14:22:17 +0800

    View_MaterialTootip
    材料显示类

---
+++ Assets/Scripts/View/Tootip/View_MaterialTootip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_MaterialTootip : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text materialAttributeOutside; //外面的文本控制大小
+    [SerializeField] private Text materialAttributeInside; //里面的文本控制显示
+    [SerializeField] private Text describeOutside; //外面的文本控制大小
+    [SerializeField] private Text describeInside; //里面的文本控制显示
+    [SerializeField] private GameObject materialAttribute; //材料的属性 如果没有,就不显示
+
+    public void InitView(Model_Item item)
+    {
+        icon.sprite = Resources.Load<Sprite>(item.sprite);
+        itemName.text = item.itemName;
+        if (item.MateriaTootip() == "")
+        {
+            materialAttribute.SetActive(false);
+        }
+        else
+        {
+            materialAttribute.SetActive(true);
+            materialAttributeOutside.text = item.MateriaTootip() + "\n";
+            materialAttributeInside.text = item.MateriaTootip();
+        }
+
+        describeOutside.text = item.description + "\n\n";
+        describeInside.text = item.description;
+    }
+}
