153ed3543a289d3b22e666acf6fe0ec04e325c28
xxslit <xxslit@Gmail.com>
2018-04-07 14:20:25 +0800

    View_ItemToolTip
    该类暂时不用 可以忽略

---
+++ Assets/Scripts/View/Prop/View_ItemToolTip.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class View_ItemToolTip : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+    }
+}
