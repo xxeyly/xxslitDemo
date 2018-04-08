69aa50d7dbc0d5331f89ed6264820bb6ec7b3d2c
xxslit <xxslit@Gmail.com>
2018-04-08 19:51:34 +0800

    Ctrl_LockedDrawing
    锁定的图纸
    打开铁匠台默认的图纸

---
+++ Assets/Scripts/Control/Anvil/Ctrl_LockedDrawing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_LockedDrawing : MonoBehaviour
+{
+
+    [SerializeField]private int itemId;//要显示的图纸
+    private Model_Item item;
+
+    public Model_Item Item
+    {
+        get
+        {
+            return item;
+        }
+
+        set
+        {
+            item = value;
+            GetComponent<View_LockedDrawing>().InitView(value);
+        }
+    }
+
+    private void Start()
+    {
+        Item = Ctrl_InventoryManager.Instance.NewItem(itemId);
+    }
+
+}
