6643cc4cae2ce915aa76b99636f66016471bd282
xxslit <xxslit@Gmail.com>
2018-04-08 20:09:03 +0800

    View_MakeTootip
    制作需求管理类的UI显示

---
+++ Assets/Scripts/View/Tootip/View_MakeTootip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_MakeTootip : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text makeCount;
+
+    public void InitView(Model_Item item)
+    {
+        icon.sprite = Resources.Load<Sprite>(item.sprite);
+        itemName.text = item.itemName;
+    }
+
+    public void UpdateMakeCount(int count)
+    {
+        makeCount.text = count.ToString();
+    }
+}
