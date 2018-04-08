ac0d99532339f791cd4e63934502535a69535045
xxslit <xxslit@Gmail.com>
2018-04-08 20:11:23 +0800

    View_MaterialItem
    暂时无用类

---
+++ Assets/Scripts/View/Tootip/View_MaterialItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_MaterialItem : MonoBehaviour
+{
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text currentItemCopunt;
+    [SerializeField] private Text demandCount;
+
+    public void InitView()
+    {
+
+    }
+}
