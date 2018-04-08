729a0453664bc449812b3511588e5e1d0d5d8a8b
xxslit <xxslit@Gmail.com>
2018-04-08 19:54:01 +0800

    Ctrl_MakeSlot
    制作格子类
    由Ctrl_MakeQueueManager统一管理

---
+++ Assets/Scripts/Control/Anvil/Ctrl_MakeSlot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_MakeSlot : MonoBehaviour
+{
+    private Model_Item item;
+    private int makeCount;
+
+    public Model_Item Item
+    {
+        get { return item; }
+
+        set
+        {
+            item = value;
+            GetComponent<View_MakeSlot>().InitView(value);
+        }
+    }
+
+    public int MakeCount
+    {
+        get { return makeCount; }
+
+        set
+        {
+            makeCount = value;
+            GetComponent<View_MakeSlot>().StartMake(makeCount);
+        }
+    }
+}
