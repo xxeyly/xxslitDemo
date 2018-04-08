6703113193089ef2f761855b867c3c02b0c7a403
xxslit <xxslit@Gmail.com>
2018-04-08 20:01:16 +0800

    View_DemandItem
    DemandItem的视图显示类

---
+++ Assets/Scripts/View/Anvil/View_DemandItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_DemandItem : MonoBehaviour
+{
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text demandCount;
+    [SerializeField] private Text currentCount;
+    /// <summary>
+    /// 初始化UI
+    /// </summary>
+    /// <param name="drawing"></param>
+    public void InitView(Model_Item.Drawing drawing)
+    {
+        itemName.text = Ctrl_InventoryManager.Instance.GetItemById(drawing.itemId).itemName;
+        demandCount.text = drawing.itemCount.ToString();
+        currentCount.text = Ctrl_BlacksmithStation.Instance.GetItemCountById(drawing.itemId).ToString();
+    }
+
+}
