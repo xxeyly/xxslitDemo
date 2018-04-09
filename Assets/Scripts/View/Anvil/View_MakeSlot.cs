edde61789b1c84ca42ae8e196916ddb33c898616
xxslit <xxslit@Gmail.com>
2018-04-09 16:05:07 +0800

    View_MakeSlot
    精简代码
    仅剩更新UI的代码

--- Assets/Scripts/View/Anvil/View_MakeSlot.cs
+++ Assets/Scripts/View/Anvil/View_MakeSlot.cs
@@ -9,65 +9,33 @@ public class View_MakeSlot : MonoBehaviour
     [SerializeField] private Image icon; //制作物品图片
     [SerializeField] private Text makeCount;
     [SerializeField] private Scrollbar makeProgress; //制作进度
-    private bool InProduction;
-    private Ctrl_MakeSlot makeSlot;
-    float makeTimer = 0;
-    private int makeAmount = 0;
-
-    private void Awake()
+    /// <summary>
+    /// 更新进度UI
+    /// </summary>
+    /// <param name="progress"></param>
+    public void UpdateMakeProgress(float progress)
     {
-        makeSlot = GetComponent<Ctrl_MakeSlot>();
+        makeProgress.size = progress;
     }
+    /// <summary>
+    /// 更新制作个数
+    /// </summary>
+    /// <param name="count"></param>
+    public void UpdateMakeCount(int count)
+    {
+        makeCount.text = count + "X";
+    }
+  
     /// <summary>
     /// UI初始化
     /// </summary>
     /// <param name="item"></param>
-    public void InitView(Model_Item item)
+    public void InitView(Model_Item item,int count)
     {
+        gameObject.SetActive(true);
         icon.sprite = Resources.Load<Sprite>(item.sprite);
-        makeCount.text = 1 + "X";
+        makeCount.text = count + "X";
         makeProgress.size = item.makeTime / item.makeTime; //设置当前制作进度为满
     }
-    /// <summary>
-    /// 开始制作协程
-    /// </summary>
-    /// <param name="count"></param>
-    public void StartMake(int count)
-    {
-        makeAmount = count;
-        makeCount.text = makeAmount + "X";
-        StartCoroutine(Make());
-    }
 
-    private void Update()
-    {
-        Debug.Log(makeAmount);
-        if (InProduction)
-        {
-            makeTimer += Time.deltaTime;
-            makeProgress.size = 1 - (makeTimer / makeSlot.Item.makeTime);
-        }
-    }
-    /// <summary>
-    /// 制作协成(稍后在做详细补充-----未完成)
-    /// </summary>
-    /// <returns></returns>
-    IEnumerator Make()
-    {
-        InProduction = true;
-        yield return new WaitForSeconds(makeSlot.Item.makeTime);
-        makeProgress.size = 1;
-        makeTimer = 0;
-        InProduction = false;
-        makeAmount--;
-        if (makeAmount <= 0)
-        {
-            StopAllCoroutines();
-            InProduction = false;
-        }
-        else
-        {
-            StartCoroutine(Make());
-        }
-    }
 }
