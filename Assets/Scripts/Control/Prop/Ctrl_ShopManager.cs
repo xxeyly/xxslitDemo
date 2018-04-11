06287f621180d5dba45e27245d2da1b08bfb6b33
xxslit <xxslit@Gmail.com>
2018-04-11 14:43:17 +0800

    Ctrl_ShopManager
    商店实例化物体改为调用对象池的方法

--- Assets/Scripts/Control/Prop/Ctrl_ShopManager.cs
+++ Assets/Scripts/Control/Prop/Ctrl_ShopManager.cs
@@ -7,15 +7,14 @@ public class Ctrl_ShopManager : Singleton<Ctrl_ShopManager>
     [SerializeField] public Transform shopItemSlots;
     
     [SerializeField] private int[] shelfGoods; //上架的商品
-
+    [SerializeField] private GameObject shopItemPre;
     
 
     private void Start()
     {
         for (int i = 0; i < shelfGoods.Length; i++)
         {
-            GameObject shopItem = Resources.Load<GameObject>("Prefabs/UI/Shop/ShopItem");
-            shopItem = Instantiate(shopItem, null);
+            GameObject shopItem = ObjectPoolTool.Instance.Pop(shopItemPre);
             shopItem.transform.parent = shopItemSlots;
             shopItem.transform.localScale = Vector3.one;
             shopItem.gameObject.GetComponent<View_ShopGroupItem>().Item =
