1ed7e9712644e590c09ac8585595bdaa5c346ffa
xxslit <xxslit@Gmail.com>
2018-04-11 14:45:55 +0800

    View_NpcTopName
    Top显示类 可以在NPC透顶显示文字

---
+++ Assets/Scripts/View/Tootip/View_NpcTopName.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View_NpcTopName : MonoBehaviour
+{
+    [SerializeField] private float visualDistance; //可视距离
+    [SerializeField] private string Name;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (visualDistance >= Vector3.Distance(this.transform.position, Camera.main.transform.position))
+        {
+            GetComponent<TextMesh>().text = Name;
+        }
+        else
+        {
+            GetComponent<TextMesh>().text = "";
+        }
+    }
+}
