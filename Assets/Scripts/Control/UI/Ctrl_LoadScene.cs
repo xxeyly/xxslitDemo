0ef4b96fdc09c8eaaa519db613076fc1510b0e2a
xxslit <xxslit@Gmail.com>
2018-04-13 18:12:21 +0800

    Ctrl_LoadScene
    开始场景,点击新游戏,加载场景

---
+++ Assets/Scripts/Control/UI/Ctrl_LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ctrl_LoadScene : MonoBehaviour {
+
+    public void OnNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
