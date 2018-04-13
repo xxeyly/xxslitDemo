a24e35f8d7a7b7c48fdb970e41a7808d8d095f2c
xxslit <xxslit@Gmail.com>
2018-04-13 18:14:37 +0800

    Model_Archiving
    数据类 存储要存储的数据
    该数据切记要和json对应,不然报错

---
+++ Assets/Scripts/Model/Other/Model_Archiving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model_Archiving
+{
+    public int x; //X轴
+    public int y; //Y轴
+    public int z; //Z轴
+    public int hp; //生命
+    public int mp; //魔法
+    public int level; //等级
+    public int exp; //经验
+    public int gold; //金币
+    public int diamod; //钻石
+    public List<Item> items = new List<Item>();
+    public List<Equip> equips = new List<Equip>();
+
+    public class Equip
+    {
+        public GlobalParametr.EquipmentType equip;
+        public int itemId;
+    }
+    public class Item
+    {
+        public int itemId;
+        public int currentCount;
+    }
+}
