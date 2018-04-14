30b3bb179480a70b3e821c30148acb32af9c1a28
xxslit <xxslit@Gmail.com>
2018-04-14 15:10:59 +0800

    Ctrl_AgreeCancelTootip
    覆盖或删除存档是弹出是否继续这样的操作

---
+++ Assets/Scripts/Control/Tootip/Ctrl_AgreeCancelTootip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ctrl_AgreeCancelTootip : MonoBehaviour
+{
+    [SerializeField] private Text titleContent;
+    public GlobalParametr.del_AgreeCancel del_AgreeCancel;
+
+    /// <summary>
+    /// 执行委托,隐藏弹窗
+    /// </summary>
+    public void OnAgree()
+    {
+        del_AgreeCancel();
+        Ctrl_TootipManager.Instance.HideAgreeCancel();
+    }
+
+    /// <summary>
+    /// 取消当前有关的委托,并隐藏当前弹窗
+    /// </summary>
+    public void OnCancel()
+    {
+        del_AgreeCancel = null;
+        Ctrl_TootipManager.Instance.HideAgreeCancel();
+    }
+
+    /// <summary>
+    /// 更改标题内容
+    /// </summary>
+    /// <param name="value"></param>
+    public void ChangeTitleContent(string value)
+    {
+        titleContent.text = value;
+    }
+}
