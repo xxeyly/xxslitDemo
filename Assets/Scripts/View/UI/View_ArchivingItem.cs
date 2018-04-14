095d7ea89bf567561b2dc5ace9d8f3440cca0948
xxslit <xxslit@Gmail.com>
2018-04-14 15:16:46 +0800

    View_ArchivingItem
    更改存档的UI显示

---
+++ Assets/Scripts/View/UI/View_ArchivingItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_ArchivingItem : MonoBehaviour
+{
+    [SerializeField] private Text index;
+    public void UpdateIndex(string value)
+    {
+        index.text = value;
+    }
+
+ 
+}
