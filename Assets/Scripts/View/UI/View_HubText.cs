c1ab7e22301cfca952d4caec088fab0d636c96b7
xxslit <xxslit@Gmail.com>
2018-04-15 15:18:25 +0800

    HUD掉血效果
    增加怪物掉血效果

---
+++ Assets/Scripts/View/UI/View_HubText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View_HubText : MonoBehaviour
+{
+    private bool isShow = false;
+    [SerializeField]private float speed = 100;
+
+    void Update()
+    {
+        if (isShow)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+    }
+
+    /// <summary>
+    /// 血条的显示
+    /// </summary>
+    public void ShowHud()
+    {
+        isShow = true;
+        StartCoroutine(AutoHideHub());
+    }
+
+    IEnumerator AutoHideHub()
+    {
+        yield return new WaitForSeconds(1f);
+        isShow = false;
+        ObjectPoolTool.Instance.Push(this.gameObject);
+    }
+}
