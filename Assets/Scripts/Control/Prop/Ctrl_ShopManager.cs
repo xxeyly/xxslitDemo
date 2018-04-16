4dfa86d7ece37918c7dc24f0c16b7529964356df
xxslit <xxslit@Gmail.com>
2018-04-16 15:31:56 +0800

    Ctrl_ShopManager
    不需要赋值对象,直接调用即可

--- Assets/Scripts/Control/Prop/Ctrl_ShopManager.cs
+++ Assets/Scripts/Control/Prop/Ctrl_ShopManager.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class Ctrl_ShopManager : Singleton<Ctrl_ShopManager>
 {
     [SerializeField] public Transform shopItemSlots;
-    
+
     [SerializeField] private int[] shelfGoods; //上架的商品
-    [SerializeField] private GameObject shopItemPre;
-    
 
     private void Start()
     {
         for (int i = 0; i < shelfGoods.Length; i++)
         {
-            GameObject shopItem = ObjectPoolTool.Instance.Pop(shopItemPre);
+            GameObject shopItem = ObjectPoolTool.Instance.Pop(ObjectPoolTool.Instance.GetObject(objPool.ShopItem));
             shopItem.transform.parent = shopItemSlots;
             shopItem.transform.localScale = Vector3.one;
             shopItem.gameObject.GetComponent<View_ShopGroupItem>().Item =
