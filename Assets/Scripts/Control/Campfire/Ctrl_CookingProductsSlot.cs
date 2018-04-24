41548d98f9544b814a2844da5571c765340cf0f2
xxslit <xxslit@Gmail.com>
2018-04-24 19:33:19 +0800

    Ctrl_CookingProductsSlot
    去掉多余注释代码,整体无变化

--- Assets/Scripts/Control/Campfire/Ctrl_CookingProductsSlot.cs
+++ Assets/Scripts/Control/Campfire/Ctrl_CookingProductsSlot.cs
@@ -17,6 +17,7 @@ public class Ctrl_CookingProductsSlot : MonoBehaviour, IPointerDownHandler
             GetComponent<View_CookingProductsSlot>().InitView(value);
         }
     }
+
     /// <summary>
     /// 更新个数显示
     /// </summary>
@@ -32,17 +33,18 @@ public class Ctrl_CookingProductsSlot : MonoBehaviour, IPointerDownHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         //手上的物品
-        Ctrl_PickUp pickUp = Ctrl_TootipManager.Instance.PickUpItem.GetComponent<Ctrl_PickUp>();
+        Ctrl_PickUp pickUp = Ctrl_TootipManager.Instance.PickUp.GetComponent<Ctrl_PickUp>();
         if (eventData.button == PointerEventData.InputButton.Right)
         {
             //手上无物品
-            if (pickUp.Item == null)
+            if (pickUp.Item == null && pickUp.Skill == null)
             {
                 pickUp.Item = Item;
                 Item = null;
             }
         }
     }
+
     /// <summary>
     /// 添加物品 食材成熟后如果当前格子为空 则新开辟一个物品地址,不为空就直接添加个数即可
     /// </summary>
@@ -55,37 +57,6 @@ public class Ctrl_CookingProductsSlot : MonoBehaviour, IPointerDownHandler
         if (Item == null)
         {
             Item = Ctrl_InventoryManager.Instance.NewItem(item.id);
-           /* Item = new Model_Item
-            {
-                id = item.id,
-                itemName = item.itemName,
-                itemType = item.itemType,
-                equipmentType = item.equipmentType,
-                materialType = item.materialType,
-                consumption = item.consumption,
-                maxStack = item.maxStack,
-                currentNumber = item.currentNumber,
-                buyPriceByGold = item.buyPriceByGold,
-                buyPriceByDiamond = item.buyPriceByDiamond,
-                sellPriceByGold = item.sellPriceByGold,
-                sellPriceByDiamond = item.sellPriceByDiamond,
-                minLevel = item.minLevel,
-                sellable = item.sellable,
-                tradable = item.tradable,
-                destroyable = item.destroyable,
-                description = item.description,
-                sprite = item.sprite,
-                useDestroy = item.useDestroy,
-                useHealth = item.useHealth,
-                useMagic = item.useMagic,
-                useExperience = item.useExperience,
-                equipHealthBonus = item.equipHealthBonus,
-                equipManaBonus = item.equipManaBonus,
-                equipDamageBonus = item.equipDamageBonus,
-                equipDefenseBonus = item.equipDefenseBonus,
-                equipSpeedcBonus = item.equipSpeedcBonus,
-                modelPrefab = item.modelPrefab
-            };*/
         }
         else
         {
